f798ed7f9fd9f32501d572edb0649ba379550b1d
AgDeSs <90023858+AgDeSs@users.noreply.github.com>
2021-11-17 21:08:28 +0300

    Add files via upload
    Add progress bar

--- scanner.py
+++ scanner.py
@@ -1,15 +1,50 @@
+from tqdm import tqdm
+
 import socket
-from threading import Thread
+import threading
+import time
 
-N = 2**16 - 1
+ip_adres = '192.0.0.8'
+thred_array = []
+closed_array = []
 
-for port in range(1,100):
-    sock = socket.socket()
+def port_scanner(ip_adres, port_name):
+    global thred_array, closed_array
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.01)
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
+        connect = sock.connect((ip_adres, port_name))
+        print(f'Порт: {port_name} открыт \n')
+        thred_array.append(port_name)
         sock.close()
+    except:
+        print(f'Порт: {port_name} закрыт \n')
+        closed_array.append(port_name)
+        sock.close()
+
+def open_ports():
+    global thred_array
+    print('Массив открытых портов:')
+    print(thred_array)
+    print(f'Открытых портов: {len(thred_array)} \n')
+
+def closed_ports():
+    global closed_array
+    closed_array.sort()
+    print('Массив закрытых портов: ')
+    print(closed_array)
+    print(f'Закрытых портов: {len(closed_array)}')
+
+for port_name in tqdm(range(1,101)):
+    thred = threading.Thread(target=port_scanner, args=(ip_adres, port_name))
+    thred.start()
+
+time.sleep(1)
+sec_thred = threading.Thread(target=open_ports)
+sec_thred.start()
+
+thrd_th = threading.Thread(target= closed_ports)
+thrd_th.start()
+
+
+
